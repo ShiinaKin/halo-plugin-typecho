f1a4d3a71ab892ca2fc03ea2e8480de0aed31cda
mashirot <shiina@sakurasou.io>
2024-05-08 08:55:46 +0800

    feat: create own starter

---
+++ src/main/java/io/sakurasou/halo/typecho/TypechoStarter.kt
@@ -0,0 +1,20 @@
+package io.sakurasou.halo.typecho
+
+import org.springframework.stereotype.Component
+import run.halo.app.plugin.BasePlugin
+import run.halo.app.plugin.PluginContext
+
+/**
+ * @author mashirot
+ * 2024/5/6 17:34
+ */
+@Component
+class TypechoStarter(pluginContext: PluginContext) : BasePlugin(pluginContext) {
+    override fun start() {
+        super.start()
+    }
+
+    override fun stop() {
+        super.stop()
+    }
+}
