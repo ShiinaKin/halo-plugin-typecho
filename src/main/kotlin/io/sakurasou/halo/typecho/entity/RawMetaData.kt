e71ed1d63b2df6999e5eeff6f2e61bca2a5d0939
mashirot <shiina@sakurasou.io>
2024-05-09 23:32:23 +0800

    feat: add RawMetaData of typecho as eneity

---
+++ src/main/kotlin/io/sakurasou/halo/typecho/entity/RawMetaData.kt
@@ -0,0 +1,14 @@
+package io.sakurasou.halo.typecho.entity
+
+/**
+ * @author mashirot
+ * 2024/5/6 23:08
+ */
+data class RawMetaData(
+    val layout: String?, val cid: Long?, val title: String?, val slug: String?, val date: String,
+    val updated: String?, val status: String?, val author: String?, val categories: List<String>?,
+    val tags: List<String>?, val customSummary: String?, val mathjax: String?,
+    val noThumbInfoEmoji: String?, val noThumbInfoStyle: String?, val outdatedNotice: String?,
+    val parseWay: String?, val reprint: String?, val thumb: String?, val thumbChoice: String?,
+    val thumbDesc: String?, val thumbSmall: String?, val thumbStyle: String?
+)
