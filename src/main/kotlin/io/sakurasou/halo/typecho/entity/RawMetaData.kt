ccafcac236446fa1b8ec7e279609cd06fe395bc2
mashirot <shiina@sakurasou.io>
2024-05-12 00:41:44 +0800

    feat: change rawMetaData date type 2 LocalDateTime

--- src/main/kotlin/io/sakurasou/halo/typecho/entity/RawMetaData.kt
+++ src/main/kotlin/io/sakurasou/halo/typecho/entity/RawMetaData.kt
@@ -1,12 +1,14 @@
 package io.sakurasou.halo.typecho.entity
 
+import java.time.LocalDateTime
+
 /**
  * @author mashirot
  * 2024/5/6 23:08
  */
 data class RawMetaData(
-    val layout: String?, val cid: Long?, val title: String?, val slug: String?, val date: String,
-    val updated: String?, val status: String?, val author: String?, val categories: List<String>?,
+    val layout: String?, val cid: Long?, val title: String?, val slug: String?, val date: LocalDateTime,
+    val updated: LocalDateTime?, val status: String?, val author: String?, val categories: List<String>?,
     val tags: List<String>?, val customSummary: String?, val mathjax: String?,
     val noThumbInfoEmoji: String?, val noThumbInfoStyle: String?, val outdatedNotice: String?,
     val parseWay: String?, val reprint: String?, val thumb: String?, val thumbChoice: String?,
--- src/main/kotlin/io/sakurasou/halo/typecho/util/ParserUtils.kt
+++ src/main/kotlin/io/sakurasou/halo/typecho/util/ParserUtils.kt
@@ -1,28 +1,43 @@
 package io.sakurasou.halo.typecho.util
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.kotlinModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.sakurasou.halo.typecho.entity.RawMetaData
 import org.apache.commons.io.FileUtils
 import java.io.File
 import java.io.IOException
 import java.nio.charset.StandardCharsets
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.function.*
 import java.util.stream.*
 
 const val PAGE = "page"
 const val POST = "post"
-val JSON_MAPPER: ObjectMapper = ObjectMapper().registerModules(JavaTimeModule(), kotlinModule())
+
+var dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+val localDateTimeModule = SimpleModule().apply {
+    addDeserializer(LocalDateTime::class.java, object : JsonDeserializer<LocalDateTime>() {
+        override fun deserialize(jp: JsonParser, ctx: DeserializationContext): LocalDateTime {
+            return LocalDateTime.parse(jp.text, dateTimeFormatter)
+        }
+    })
+}
+
+val JSON_MAPPER: ObjectMapper = ObjectMapper().registerModules(JavaTimeModule(), kotlinModule(), localDateTimeModule)
 
 /**
  * @author mashirot
  * 2024/5/6 22:26
  */
 object ParserUtils {
-    private val YAML_MAPPER = YAMLMapper().registerKotlinModule()
+    private val YAML_MAPPER = YAMLMapper().registerModules(JavaTimeModule(), kotlinModule(), localDateTimeModule)
     private val TYPE_ARR_MD = arrayOf("md")
 
     @Throws(IOException::class)
@@ -54,19 +69,17 @@ object ParserUtils {
 
     @Throws(IOException::class)
     private fun parseFolder(folder: File): List<Pair<RawMetaData, String>> {
-        val files = ArrayList(
-            FileUtils.listFiles(folder, TYPE_ARR_MD, true)
-        )
-        val parseResult: MutableList<Pair<RawMetaData, String>> = ArrayList(files.size + 1)
-        for (file in files) {
-            val rawContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8)
-            val beginIndex = rawContent.indexOf("---")
-            val endIdx = rawContent.indexOf("---", 3)
-            val metaDataStr = rawContent.substring(beginIndex + 3, endIdx)
-            val content = rawContent.substring(endIdx + 3)
-            val metaData = YAML_MAPPER.readValue(metaDataStr, RawMetaData::class.java)
-            parseResult.add(Pair(metaData, content))
+        val files = ArrayList(FileUtils.listFiles(folder, TYPE_ARR_MD, true))
+        return ArrayList<Pair<RawMetaData, String>>(files.size + 1).apply {
+            for (file in files) {
+                val rawContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8)
+                val beginIndex = rawContent.indexOf("---")
+                val endIdx = rawContent.indexOf("---", 3)
+                val metaDataStr = rawContent.substring(beginIndex + 3, endIdx)
+                val content = rawContent.substring(endIdx + 3)
+                val metaData = YAML_MAPPER.readValue(metaDataStr, RawMetaData::class.java)
+                add(Pair(metaData, content))
+            }
         }
-        return parseResult
     }
 }
