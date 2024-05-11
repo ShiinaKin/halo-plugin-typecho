1e75361e2535ac67e79a931c16f87d73cfa99690
mashirot <shiina@sakurasou.io>
2024-05-12 00:40:26 +0800

    feat: add timeZone 2 plugin setting

--- src/main/kotlin/io/sakurasou/halo/typecho/service/ConfigServiceImpl.kt
+++ src/main/kotlin/io/sakurasou/halo/typecho/service/ConfigServiceImpl.kt
@@ -7,13 +7,14 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
 import run.halo.app.extension.ConfigMap
 import run.halo.app.extension.ExtensionClient
+import java.time.ZoneId
 
 /**
  * @author mashirot
  * 2024/5/9 22:33
  */
 @Service
-class PATServiceImpl(
+class ConfigServiceImpl(
     private val extensionClient: ExtensionClient,
 ) {
     private val logger = KotlinLogging.logger { this::class.java }
@@ -25,5 +26,17 @@ class PATServiceImpl(
         val pat = basicGroup["pat"] as String?
         return pat?.let { it.ifBlank { throw RuntimeException("No PAT") } }
             ?: throw RuntimeException("No PAT")
+    }
+
+    fun getTimeZone(): ZoneId {
+        val configMap = extensionClient.fetch(ConfigMap::class.java, "plugin-typecho-configmap").get()
+        val basicGroup = jsonMapper.readValue(configMap.data["basic"], Map::class.java)
+        val timeZone = basicGroup["timeZone"] as String?
+        return timeZone?.let {
+            it.ifBlank { throw RuntimeException("No TimeZone") }
+            runCatching {
+                ZoneId.of(it)
+            }.getOrElse { throw RuntimeException("Wrong TimeZone") }
+        } ?: throw RuntimeException("No TimeZone")
     }
 }
