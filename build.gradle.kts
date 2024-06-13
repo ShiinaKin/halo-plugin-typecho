99917c4cbe599e89bfd8fa546bc7c1a2212d6c04
Shiina Kin <shiina@sakurasou.io>
2024-06-13 10:55:12 +0800

    chore: update halo dev plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "2.0.0"
     id("com.github.node-gradle.node") version "7.0.2"
-    id("run.halo.plugin.devtools") version "0.0.8-SNAPSHOT"
+    id("run.halo.plugin.devtools") version "0.0.9"
 }
 
 group = "io.sakurasou.halo.typecho"
